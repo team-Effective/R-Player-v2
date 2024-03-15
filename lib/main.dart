2e6bdf696f3f3038885ffe430452436282051cb2
nonrita <2220025@ecc.ac.jp>
2024-03-15 19:07:46 +0900

    add:Bluetooth接続処理の追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:r_player/Controller/bluetooth_controller.dart';
+import 'package:r_player/model/bluetooth.dart';
 import 'package:r_player/view/connect_page.dart';
 import 'package:r_player/view/game_result_page.dart';
 import 'package:r_player/view/home_page.dart';
@@ -6,6 +8,7 @@ import 'package:r_player/view/login_page.dart';
 import 'package:r_player/view/mission_details_page.dart';
 import 'package:r_player/view/mission_list_page.dart';
 import 'package:r_player/view/mission_result_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,18 +20,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => const LoginPage(),
-        '/connect': (context) => const ConnectPage(),
-        '/home': (context) => const HomePage(),
-        '/mission/list': (context) => const MissionListPage(),
-        '/mission/details': (context) => const MissionDetailsPage(),
-        '/mission/result': (context) => const MissionResultPage(),
-        '/game/result': (context) => const GameResultPage(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<Bluetooth>(
+          create: (context) => Bluetooth(),
+        ),
+        ProxyProvider<Bluetooth, BluetoothController>(
+          update: (_, bluetooth, __) => BluetoothController(bluetooth),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/login',
+        routes: {
+          '/login': (context) => const LoginPage(),
+          '/connect': (context) => const ConnectPage(),
+          '/home': (context) => const HomePage(),
+          '/mission/list': (context) => const MissionListPage(),
+          '/mission/details': (context) => const MissionDetailsPage(),
+          '/mission/result': (context) => const MissionResultPage(),
+          '/game/result': (context) => const GameResultPage(),
+        },
+      ),
     );
   }
 }
